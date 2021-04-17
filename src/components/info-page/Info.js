4f3658202bbd9afb0ccc373cc9a9bc275cd1cb0a
Jurakulov Zarrukh <74851684+Zarrukh-Jurakulov@users.noreply.github.com>
2021-04-17 15:24:11 +0500

    feat: info page added

--- src/components/info-page/Info.js
+++ src/components/info-page/Info.js
@@ -4,7 +4,38 @@ import './info.css'
 const Info = () => {
     return (
         <div className='infoPage'>
-            
+            <div className="infoPageContext">
+                <h1>Heading</h1>
+                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Cupiditate ullam blanditiis iste quasi est, voluptate, maiores illo quae dolores, quisquam molestias eum?
+                Commodi cum praesentium veniam at facere illum rem. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Corrupti nemo quas perferendis, alias commodi beatae eos at mollitia nihil, vero laboriosam veritatis magnam?
+                Fuga pariatur impedit consequatur deserunt labore maxime! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quia voluptate odit facere vel non voluptatem placeat necessitatibus rem. Necessitatibus et quidem veritatis enim amet excepturi officiis.
+                    Hic velit dolor omnis.</h3>
+
+                <h1>Heading</h1>
+                <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Dignissimos id ipsam rerum ullam debitis ratione est facilis tempore.
+                Autem voluptates ea, non impedit illo excepturi est possimus perferendis.
+                Obcaecati, repudiandae? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nostrum blanditiis vero recusandae, possimus iure officiis iusto porro laboriosam et nulla minus. Quae fuga perspiciatis asperiores cupiditate veniam nisi,
+                sapiente recusandae. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Quidem quos voluptas quo nostrum harum expedita magni totam ex.
+                Harum impedit quaerat vero eum est numquam cum a in sunt molestiae. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quas sint facere quasi praesentium suscipit pariatur cupiditate blanditiis dicta modi dolore eligendi voluptas asperiores quibusdam quidem cumque sed,
+                 numquam temporibus laboriosam!</h3>
+
+                <h1>Heading</h1>
+                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Cupiditate ullam blanditiis iste quasi est, voluptate, maiores illo quae dolores, quisquam molestias eum?
+                Commodi cum praesentium veniam at facere illum rem. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Corrupti nemo quas perferendis, alias commodi beatae eos at mollitia nihil, vero laboriosam veritatis magnam?
+                Fuga pariatur impedit consequatur deserunt labore maxime!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fugit expedita totam numquam accusantium necessitatibus sed voluptates, eaque laboriosam modi obcaecati at soluta, ab eius.
+                    Maxime excepturi ipsa soluta cumque!</h3>
+            </div>
+
         </div>
     )
 }
