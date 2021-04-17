91c5a557423a37d9bf2cfef240d025412f24a883
ZarrukhJurakulov <zarrukhjurakulov474@gmail.com>
2021-04-17 14:48:21 +0500

    feat: sidebar added

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -6,38 +6,28 @@ import info from '../../assets/info.png'
 import help from '../../assets/help.png'
 import {Link} from "react-router-dom";
 import Modal from './Modal'
-import useModal from './useModal'
 
 const Sidebar = ({toggle, visible}) => {
 
-  
-
-//   function  keyDownHandler(e){
-//         if(e.keyCode === 13 && e.ctrlKey) {
-//             this.showMessage()
-//         }
-//       }
-//     function  componentDidMount() {
-//         document.addEventListener("keydown", this.keyDownHandler)
-//       }
-//     function  componentWillUnmount(){
-//         document.removeEventListener('keydown',this.keydownHandler);
-//       }
-    
     return(
         <aside className='sidebar'>
-            <img src={logo} alt="logo"/>
-            <ul className='sidearContext'>
-                <Link to='/'><li><img src={home} alt="home"/> MAIN</li></Link>   
-                <Link to='/info'> <li><img src={info} alt=""/> INFO</li></Link> 
-                <Link to='/help'><li><img src={help} alt=""/> HELP</li></Link>  
-                 <Modal  visible={visible} toggle={toggle} />
-                  <button onClick={toggle}>Show Modal</button>
-                  <p onKeyUp={toggle}>Ctrl  + ENTER</p>
-            </ul>
+            <img src={logo} className="chatLogo" alt="logo"/>
+            <div className='sidebarContext'>
+                <div className="pagesContainer">
+                    <img src={home} className="pagesLogo" alt="home"/><Link to='/'>MAIN</Link>     
+                </div>
+                <div className="pagesContainer">
+                    <img src={info} className="pagesLogo" alt="info"/><Link to='/info'>INFO</Link>  
+                </div>
+                <div className="pagesContainer">
+                    <img src={help} className="pagesLogo" alt="help"/><Link to='/help'> HELP</Link>   
+                </div>
+                
+               
+            </div>
+             <Modal  visible={visible} toggle={toggle} />
+                <p onKeyUp={toggle} className="ctrlPlusenter">Ctrl  + ENTER</p>
 
-           
-          
         </aside>
     )
 }
